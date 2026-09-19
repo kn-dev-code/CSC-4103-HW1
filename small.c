dee27d2ed6341472fb6c9adc0060d5e1fa656bb3
Kyle <nettervillekyle@gmail.com>
2026-09-18 21:39:16 -0500

    feat: Created X panel

---
+++ small.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    for (int i = 0; i < 55; i++) {
+        if (i % 11 == 10) {
+            putchar('\n');
+        } else if (i % 2 == 0) {
+            putchar(' ');
+        } else {
+            putchar('X');
+        }
+    }
+    return 0;
+}
